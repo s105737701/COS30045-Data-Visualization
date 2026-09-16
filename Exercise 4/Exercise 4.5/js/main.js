20c330e7cc416b70848d63fea186edbfab34b3e4
s105737701 <105737701@student.swin.edu.au>
2026-09-16 20:09:04 +0800

    4.5 done

--- Exercise 4/Exercise 4.5/js/main.js
+++ Exercise 4/Exercise 4.5/js/main.js
@@ -1,27 +1,84 @@
 const svg = d3.select(".responsive-svg-container")
-    .append("svg")
-      .attr("viewBox", "0 0 1200 1600")
-      .style("border", "1px solid black");
 
-svg
-  .append("rect")
-    .attr("x", 10)
-    .attr("y", 10)
-    .attr("width", 414)
-    .attr("height", 16)
-    .attr("fill", "blue");
+  .append("svg")
+
+    .attr("viewBox", "0 0 1200 1600")
+
+    .style("border", "1px solid black");
+
+
+ 
 
 d3.csv("data/4.4exportdata.csv", d => {
+
   return {
+
     brand: d.brand,
+
     count: +d.count
+
   };
+
 }).then(data => {
+
   console.log(data);
+
   console.log(data.length);
+
   console.log(d3.max(data, d => d.count));
+
   console.log(d3.min(data, d => d.count));
-  console.log(d3.extent(data, d => d.count)); //=> array with min and max
+
+
+ 
+
   data.sort((a, b) => b.count - a.count);
+
+
+ 
+
   drawBarChart(data);
+
+  
+
 });
+
+
+ 
+
+const drawBarChart = data => {
+
+  const barHeight = 20;
+
+  const barSpacing = 5;
+
+
+ 
+
+  svg
+
+    .selectAll("rect")
+
+    .data(data)
+
+    .join("rect")
+
+    .attr("class", d => {
+
+      console.log(d);
+
+      return `bar bar-${d.count}`;
+
+    })
+
+    .attr("width", d => d.count)
+
+    .attr("height", barHeight)
+
+    .attr("fill", "blue")
+
+    .attr("x", 0)
+
+    .attr("y", (d, i) => i * (barHeight + barSpacing));
+
+};
